04c3473e746de7e8e43c377b2f85dddf36a3147d
tlrdmsEjrqhRdl <s24030@gsm.hs.kr>
2025-01-03 11:28:34 +0900

    start

---
+++ app/pages/calandar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import * as S from '../styles/calandar';
+
+function calandar() {
+  return (
+    <>
+        <S.Background>
+          <S.CalandarContainer></S.CalandarContainer>
+        </S.Background>
+    </>
+  )
+}
+
+export default calandar
---
+++ app/styles/calandar.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Background = styled.div`
+    width: 100%;
+    height: 100%;
+    background-color: white;
+    position: absolute;
+    left: 0px;
+    top: 0px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+export const CalandarContainer = styled.div`
+    width: 700px;
+    height: 100%;
+    border-width: 0px 2px 2px 0px;
+    border: solid black;
+`
+
