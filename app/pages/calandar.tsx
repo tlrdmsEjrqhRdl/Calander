49bf276dfb46c102e2321808a43545fd3a75c99c
tlrdmsEjrqhRdl <s24030@gsm.hs.kr>
2025-01-06 23:18:29 +0900

    a

--- app/pages/calandar.tsx
+++ app/pages/calandar.tsx
@@ -3,9 +3,18 @@ import * as S from '../styles/calandar';
 
 const today = new Date();
 const year = today.getFullYear();
+const month = today.getMonth() + 1;
 
-function BeforeYear(year: any){
-  console.log(year);
+function BeforeMonth(year: number, month: number): { year: number; month: number } {  //연도 & 달 감소소
+  if (month === 1) {
+    return { year: year - 1, month: 12 }; // 이전 달이 12월이고 연도는 감소
+  } else {
+    return { year, month: month - 1 }; // 단순히 월만 감소
+  }
+}
+
+function afterMonth(month: any){
+
 }
 function calandar() {
   return (
@@ -13,15 +22,15 @@ function calandar() {
         <S.Background>
           <S.CalandarContainer>
             <S.CalandarHeader>
-              <S.backyear onClick={BeforeYear}>⟨</S.backyear>
-              <S.CalandarYear>{year}</S.CalandarYear>
-              <S.nextyear>⟩</S.nextyear>
+              <S.backyear onClick={BeforeMonth}>⟨</S.backyear>
+              <S.CalandarYear>{year}.{month}</S.CalandarYear>
+              <S.nextyear onClick={afterMonth}>⟩</S.nextyear>
             </S.CalandarHeader> 
 
           </S.CalandarContainer>
         </S.Background>
     </>
-  )
+  ) 
 }
 
 export default calandar
